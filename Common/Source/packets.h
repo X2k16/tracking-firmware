953f7e71fd123f2b405581ec7f08c4cca8594155
misodengaku <misodengaku@gmail.com>
2016-01-23 23:30:41 +0900

    add missing files

---
+++ Common/Source/packets.h
@@ -0,0 +1,45 @@
+
+
+
+#ifndef PACKETS_H_
+#define PACKETS_H_
+
+#include "ToCoNet_packets.h"
+
+typedef enum
+{
+	PACKET_CMD_QUESTION = TOCONET_PACKET_CMD_APP_USER,
+	PACKET_CMD_POWER,
+	PACKET_CMD_ANSWER,
+	PACKET_CMD_REQUEST,
+	PACKET_CMD_RESPONSE,
+	PACKET_CMD_STATUS
+} tePacketCmdApp;
+
+typedef enum
+{
+	PACKET_CBID_QUESTION = 0x80,
+	PACKET_CBID_POWER,
+	PACKET_CBID_ANSWER,
+	PACKET_CBID_REQUEST,
+	PACKET_CBID_RESPONSE,
+	PACKET_CBID_STATUS
+} tePacketCbid;
+
+
+typedef struct
+{
+	char IDm[8];
+} tsFelicaData;
+
+
+
+#endif /* PACKETS_H_ */
+
+
+
+
+enum PacketType
+{
+	Data, Cmd, Felica
+};
